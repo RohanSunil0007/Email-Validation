c443da2fe289be1a690a1c074d2e60c2453534f1
Rohan Sunil <reach.rohan0007@gmail.com>
2020-10-03 00:07:09 +0530

    Added : optional functionality

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "Welcome to Email Validator"
 read -p "Enter email : " email
-pat="^[a-zA-Z0-9]+([-._+][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}"
+pat="^[a-zA-Z0-9]+([-._+][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-z]{2,3})*$"
 if [[ $email =~ $pat ]]
 then
    echo "Valid"
