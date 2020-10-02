8a7dfd22bb924fc2644c9a6451bc7f0d0d863cb4
Rohan Sunil <reach.rohan0007@gmail.com>
2020-10-02 19:38:52 +0530

    Added : Top Level Domain Functionality

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Welcome to Email Validator"
 read -p "Enter email : " email
-pat="^[a-zA-Z0-9]+([._-+][0-9a-zA-Z]+)*@[0-9a-zA-Z]"
+pat="^[a-zA-Z0-9]+([-._+][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}"
 if [[ $email =~ $pat ]]
 then
    echo "Valid"
